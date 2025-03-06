a6f85e627d1fe546553617aa306dac08f1a80ef4
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-06 17:37:15 +0530

    Update Deck.java
    Added functionality to create a deck

--- Deck.java
+++ Deck.java
@@ -9,5 +9,17 @@ class Deck {
 		deck = new ArrayList<>();	
 	}
 	
+	public void createDeck(){
+		String[] ranks = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+		String[] suits = {"Hearts", "Spades", "Diamonds", "Clubs"};
+		for (String rank: ranks){
+			for(String suit: suits){
+				deck.add(new Card(rank,suit));
+			}
+		}
+						
+	}
 	
+	
+
 }
