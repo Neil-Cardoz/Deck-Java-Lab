e8c255bd3328e7648c52d3657860c2ff57f1cc6e
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-06 17:54:33 +0530

    Create Main.java
    Innitialized Deck object

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Deck deck = new Deck();
+        deck.createDeck(); // Initialize the deck with cards
+
+}
